c83580453e60e72bc3dbb9af9217acadf13f4384
Renan Biegelmeyer <renan.biegel@gmail.com>
2020-09-01 22:47:32 -0300

    returning to sqlite

--- pdv/domain/establishment_service.py
+++ pdv/domain/establishment_service.py
@@ -12,8 +12,8 @@ class EstablishmentService:
     def create_establishment(self, establishment: Establishment) -> None:
         self.repository.save(establishment)
 
-    def get_establishments(self):
+    def get_establishments(self) -> list:
         return self.repository.list()
 
-    def get_establishment(self, cnpj: str):
+    def get_establishment(self, cnpj: str) -> Establishment:
         return self.repository.find_by_cnpj(cnpj)
